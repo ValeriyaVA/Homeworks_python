6112cd8d353c972ba5220e6537d2f03613588c83
ValeriyaVA <97730198+ValeriyaVA@users.noreply.github.com>
2022-01-23 00:03:16 +0300

    версия HW_2 через функции, неконечная добавила первую задачу исправила косяк с нулевой температурой во второй

--- Les_2/2_4.py
+++ Les_2/2_4.py
@@ -1,9 +1,10 @@
-# элементы списка - строки из нескольких слов, где последнее имя. Вывести "Привет, {имя}!" для всего списка имён
+# элементы списка - строки из нескольких слов, где последнее имя, которое отделено пробелом. Вывести список из приветствий для каждого имени
 
 def convert_name_extract(list_in: list) -> list:
-    for word in employees_list:
     list_out = []
-    list_out.append('Привет,' + word[word.rfind(' '):].title()+'!')
+    for word in list_in:
+        list_out.append('Привет,' + word[word.rfind(' '):].title()+'!')
+    return list_out
 
 my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
 result = convert_name_extract(my_list)
--- Les_2/2_5.py
+++ Les_2/2_5.py
@@ -21,11 +21,13 @@ def transfer_list_in_str(list_in: list) -> str:
 def sort_prices(list_in: list) -> list:
     """Сортирует вещественные числа по возрастанию, не создавая нового списка"""
     list_in.sort()
+    return list_in
 
 def sort_price_adv(list_in: list) -> list:
     """Создаёт новый список и возвращает список с элементами по убыванию"""
     sort_prices(list_in)
     list_in.reverse()
+    return list_in
 
 def check_five_max_elements(list_in: list) -> list:
     """Проверяет элементы входного списка вещественных чисел и возвращает
@@ -40,12 +42,17 @@ my_list = [round(uniform(10, 100), 2) for _ in range(1, 16)]  # автомати
 print(f'Исходный список: {my_list}')
 result_1 = transfer_list_in_str(my_list)
 print(result_1)
+
 id_my_list = id(my_list)
-sort_prices(my_list)
-print(my_list)
-id_sorted_my_list= id(my_list) # зафиксируйте здесь доказательство, что результат result_2 остался тем же объектом
-sort_price_adv(my_list)
-print(id_sorted_my_list == id_my_list)
-print(my_list)
+
+result_2 = sort_prices(my_list)
+print(result_2)
+id_sorted_my_list = id(result_2)
+
+print(id_sorted_my_list == id_my_list) # зафиксируйте здесь доказательство, что результат result_2 остался тем же объектом
+
+result_3 = sort_price_adv(my_list)
+print(result_3)
+
 result_4 = check_five_max_elements(my_list)
 print(result_4)
