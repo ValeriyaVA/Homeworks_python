bbedabecf974a863c4a207b32e6e880f4059c602
ValeriyaVA <97730198+ValeriyaVA@users.noreply.github.com>
2022-01-24 01:05:52 +0300

    3_3 modified

--- Les_3/3_3.py
+++ Les_3/3_3.py
@@ -1,22 +1,25 @@
-# def thesaurus(*args):
-#     names = {}
-#     for i in args:
-#         if i[0] in names.keys():
-#             names[i[0]] += [i]
-#         else:
-#             names[i[0]] = [i]
-#     return names
+def thesaurus(*args):
+    names = {}
+    print(args, 'the', '\n')
+    for i in args:
+        if i[0] in names.keys():
+            names[i[0]] += [i]
+        else:
+            names[i[0]] = [i]
+    return names
 
 
 def thesaurus_adv(*args):
     surnames = {}
-    names = {}
     for i in args:
         Name, Surname = (str(j) for j in i.split())
         if Surname[0] in surnames.keys():
-            surnames[Surname[0]] += [i]
+            surnames[Surname[0]] += {i}
         else:
             surnames[Surname[0]] = [i]
+    for i in surnames.keys():
+        lst = thesaurus(surnames[i])
+        print(lst, 'adv')
     return surnames
 
 result = thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
