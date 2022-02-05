23af2c2e34147b0c22c6f39dcd7762a952ba71e3
ValeriyaVA <97730198+ValeriyaVA@users.noreply.github.com>
2022-02-05 20:43:44 +0300

    6_4 запись в файл

--- Les_6/Task_6_4.py
+++ Les_6/Task_6_4.py
@@ -2,23 +2,26 @@ import sys
 import json
 
 
-def prepare_dataset(path_users_file: str, path_hobby_file: str):
+def create_user_hobby(path_users_file: str, path_hobby_file: str):
     """
     Считывает данные из файлов и сохраняет объединенные данные в новый файл users_hobby.txt
     :param path_users_file: путь до файла, содержащий ФИО пользователей, разделенных запятой по строке
     :param path_hobby_file: путь до файла, содержащий хобби, разделенные запятой по строке
     :return: файл users_hobby.txt
     """
-    user_file = open('users.csv', 'r', encoding='utf-8')
-    hobby_file = open('hobby.csv', 'r', encoding='utf-8')
+    user_file = open(path_users_file, 'r', encoding='utf-8')
+    hobby_file = open(path_hobby_file, 'r', encoding='utf-8')
+    users_hobby = open('users_hobby.txt', 'w', encoding='utf-8')
     if sum(1 for line_u in open('users.csv', 'r', encoding='utf-8')) < sum(
             1 for line_h in open('hobby.csv', 'r', encoding='utf-8')):
         sys.exit([1])
     elif sum(1 for line_u in open('users.csv', 'r', encoding='utf-8')) >= sum(
             1 for line_h in open('hobby.csv', 'r', encoding='utf-8')):
+        for line in hobby_file:
+            print("{}: {}".format(user_file.readline().rstrip(), line), file=users_hobby, end='')
+        print(file=users_hobby)
+        for line in user_file:
+            print("{}: {}".format(line.rstrip(), None), file=users_hobby, end='\n')
 
-        user_lines = user_file.readline()
-        hobby_lines = hobby_file.readline()
 
-
-return dict_user_hobby
+create_user_hobby('users.csv', 'hobby.csv')
