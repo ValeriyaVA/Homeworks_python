6dbe5513c677b2516a4ab8d5ae7855964558bd1e
ValeriyaVA <97730198+ValeriyaVA@users.noreply.github.com>
2022-02-06 20:44:43 +0300

    change_sale скачет на костылях с консоли

--- Les_6/change_sale.py
+++ Les_6/change_sale.py
@@ -5,10 +5,9 @@ sales = open('bakery.csv', 'r+', encoding='utf-8')
 
 def seek_move(seek: int):
     '''
-    Функция переводит курсор в файле на заданный с клавиатуры номер строки и возвращает
-    кортеж с заменяемой строкой и курсором
+    Функция переводит курсор в файле на заданный с клавиатуры номер строки и возвращает заменяемую строку
     :param seek: номер строки, на которую нужно перевести курсор
-    :return: заменяемая строка и курсор на неё'''
+    :return: заменяемая строка'''
     i = 2
     while i <= seek:
         sales.readline()
@@ -22,21 +21,22 @@ def change_sale(number: int, new_sale: str):
     :param number: номер строки, в которой хотим внести изменение
     :param new_sale: значение, на которое хотим заменить
     """
+    if number > sum(1 for line in open('bakery.csv')):
+        sys.exit(['Количество строк меньше заданного числа, изменение невозможно'])
     old_sale = seek_move(number)
-    print(old_sale)
     replaced_content = ""
-    sales.seek(sales.tell() - 22)
+    start_change = sales.tell() - 22
+    sales.seek(start_change)
     i = sales.tell() // 22
-    print(i)
     while i + 1 <= sum(1 for line in open('bakery.csv')):
         line = sales.readline()
         new_line = line.replace(old_sale, new_sale.ljust(20)+'\n')
         replaced_content = replaced_content + new_line
         i += 1
-    print(replaced_content)
-    # sales.write(replaced_content)
+    sales.seek(start_change)
+    sales.write(replaced_content)
 
-change_sale(8, '123456')
+# change_sale(8, '123456')
 # вообще тут надо ловить ошибку ValueError: could not convert string to float или int, но этого не было в лекциях
-# if int(sys.argv[1]) and (float(sys.argv[2].replace(',', '.')) or int(sys.argv[2])):
-#     change_sale(int(sys.argv[1]), sys.argv[2])
+if int(sys.argv[1]) and (float(sys.argv[2].replace(',', '.')) or int(sys.argv[2])):
+    change_sale(int(sys.argv[1]), sys.argv[2])
