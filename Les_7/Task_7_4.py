61efe18e7f1b090195d13efe7289c46847f1c246
ValeriyaVA <97730198+ValeriyaVA@users.noreply.github.com>
2022-02-09 21:04:08 +0300

    7_4 working

---
+++ Les_7/Task_7_4.py
@@ -0,0 +1,35 @@
+import os
+
+
+def get_dict_size(path: str):
+    '''
+    Функция, которая выводит статистику для заданной папки в виде словаря,
+    в котором ключи — верхняя граница размера файла (кратна 10),
+    а значения — общее количество файлов (в том числе и в подпапках), размер которых не превышает этой границы,
+    но больше предыдущей
+    :param path: путь до папки, в которой будем ковыряться
+    :return Dict(size: int)
+    '''
+    NUMBER = 10
+    dict_sizes = {10 ** i: 0 for i in range(2, NUMBER)}
+    list_keys = list(dict_sizes.keys())
+    if not os.path.exists(path):
+        print('This path does not exist!')
+        return False
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            # size = os.path.getsize(root + '\\' + file)
+            size = os.stat(os.path.join(path, file)).st_size
+            # print(root + '\\' + file + ' Размер: % s байт' % size)
+            if size <= list_keys[0]:
+                dict_sizes[list_keys[0]] += 1
+            if list_keys[-1] <= size:
+                dict_sizes[list_keys[-1]] += 1
+            for i in range(1, len(list_keys)):
+                if list_keys[i - 1] <= size <= list_keys[i + 1]:
+                    dict_sizes[list_keys[i]] += 1
+                    break
+    return dict_sizes
+
+
+print(get_dict_size('D:\\Homeworks_python\\Les_7'))
