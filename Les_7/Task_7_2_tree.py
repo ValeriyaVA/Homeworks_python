6244eb6009e4d3aaeff359d3811d81bc2b0d1c00
ValeriyaVA <97730198+ValeriyaVA@users.noreply.github.com>
2022-02-09 10:59:29 +0300

    7_2 что-то работает, всё ещё нужны костыли

--- Les_7/Task_7_2_tree.py
+++ Les_7/Task_7_2_tree.py
@@ -2,29 +2,43 @@ import os
 
 TEMPLATE = '    '
 def make(path_dir: str, structure: str):
-
+    '''
+    Функция, которая идёт в заданной папке создаёт структуру папок из файла config.yaml
+    Структура: корневые папки записаны без знаков-разделителей в начале.
+    Папки одного уровня находятся соответственно на кратном расстоянии по разделителю TEMPLATE = '    '.
+    В структуре может присутствовать два типа файлов: *.py и *.html
+    :param path_dir: путь, куда создать дерево папок с файлами из config.yaml
+    :param structure: путь до файла со структурой
+    '''
     with open(structure, 'r', encoding='utf-8') as fr:
-        j = 0
-        while j <= sum(1 for line in structure):
-            parent_name = fr.readline().replace('\n', '')
+        parent_name = fr.readline().replace('\n', '')
+        while parent_name:
             print(parent_name)
             if not parent_name.startswith(TEMPLATE):  # обрабатываем корневые папки
                 os.mkdir(os.path.join(path_dir, parent_name))
                 os.chdir(os.path.join(path_dir, parent_name))
                 first_dir = (os.getcwd())
-                j += 1
+                parent_name = fr.readline().replace('\n', '')
             else:
                 i = 1
                 while True:
-                    print(os.getcwd(), i)
-                    j += 1
-                    if parent_name.startswith(TEMPLATE * i):
-                        child_dir_name = parent_name.strip()
+                    print(os.getcwd(), 'куда пишем', i)
+                    if parent_name.startswith(TEMPLATE * i) and not os.path.exists(parent_name.strip(TEMPLATE * i)):
+                        child_dir_name = parent_name.strip(TEMPLATE * i)
                         os.mkdir(os.path.join(child_dir_name))
-                        os.chdir(os.path.join(child_dir_name))
-                        i += 1
-                    else:
-                        os.chdir(os.path.join(path_dir, first_dir))
+                        print(os.getcwd())
+                        parent_name = fr.readline().replace('\n', '')
+                        print(parent_name)
+                        if parent_name.startswith(TEMPLATE * (i+1)):
+                            i += 1
+                            os.chdir(os.path.join(child_dir_name))
+                    elif os.path.exists(os.path.join(path_dir, first_dir, parent_name.strip(TEMPLATE))):
+                        parent_name = fr.readline()
+                        break
+                    elif parent_name.startswith(TEMPLATE) and not parent_name.startswith(TEMPLATE*2):
+                        print(parent_name, 'откат')
+                        os.mkdir(os.path.join(path_dir, first_dir, parent_name.strip(TEMPLATE)))
+                        os.chdir(os.path.join(path_dir, first_dir, parent_name.strip(TEMPLATE)))
                         break
 
 
